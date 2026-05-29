a6300af1d05601f0ff893fd7a8230b2130574c48
dhanusrisasikumar <dhanusrisasikumar2592@gmail.com>
2026-05-29 16:35:00 +0530

    first commit

---
+++ loop.py
@@ -0,0 +1,14 @@
+students=["abi","anu","priya","madhu","sri"]
+departments=["cse","it","mech","eee","ece"]
+
+def student_details(name,department):
+   
+   students=["abi","anu","priya","madhu","sri"]
+   departments=["cse","it","mech","eee","ece"]
+
+
+for i in range (len(students)):
+    print("student name:",students[i])
+    print("student department:",departments[i])
+    print("------------------------------")
+   
